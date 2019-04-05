4e2fb9a59a1cbe812f2a26e18d838778a455ea09
feiya1314 <yufei1313@gmail.com>
2019-04-06 00:53:37 +0800

    add win10 register

---
+++ win1pregister/registerVMWARE.ps1
@@ -0,0 +1,4 @@
+cd ./binaries/Windows/intel
+.\/vlmcs-Windows-x64.exe  -v -l 3 192.168.3.8
+
+pause
---
+++ win1pregister/registerVPS.ps1
@@ -0,0 +1,4 @@
+cd ./binaries/Windows/intel
+.\/vlmcs-Windows-x64.exe  -v -l 3 45.78.33.57
+
+pause
